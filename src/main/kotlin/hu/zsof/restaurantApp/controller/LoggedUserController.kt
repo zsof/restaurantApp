28cb65bb6cd6773d9e7a5f6de159d6ab3ec5df66
Kárpáti Zsófia Viktória <zsofi.viki@gmail.com>
2022-10-14 11:05:22 +0200

    add todo rename endpoint

--- src/main/kotlin/hu/zsof/restaurantApp/controller/LoggedUserController.kt
+++ src/main/kotlin/hu/zsof/restaurantApp/controller/LoggedUserController.kt
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/user")  //Todo lehetne valami loggedUser
 class LoggedUserController(
     private val userService: UserService,
     private val placeService: PlaceService
