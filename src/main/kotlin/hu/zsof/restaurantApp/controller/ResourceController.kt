a53b77df639ba5823ab6c18ccfec6c008da27f6a
zsof <zsofi.viki@gmail.com>
2022-12-12 21:09:04 +0100

    Fix image post, Create image get request

--- src/main/kotlin/hu/zsof/restaurantApp/controller/ResourceController.kt
+++ src/main/kotlin/hu/zsof/restaurantApp/controller/ResourceController.kt
@@ -6,8 +6,10 @@ import hu.zsof.restaurantApp.repository.UserRepository
 import hu.zsof.restaurantApp.util.AuthUtils
 import hu.zsof.restaurantApp.util.Constants
 import net.bytebuddy.utility.RandomString
+import org.springframework.core.io.UrlResource
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.util.StringUtils
 import org.springframework.web.bind.annotation.*
@@ -25,22 +27,26 @@ class ResourceController(private val placeRepository: PlaceRepository, private v
     fun newPlace(
             @RequestParam("image") file: MultipartFile,
             // PlaceId or UserId
-            @RequestParam("typeId") typeId: Long,
+            @RequestParam("typeId") typeId: String,
             // place or user
             @RequestParam("type") type: String,
             @CookieValue(AuthUtils.COOKIE_NAME) token: String?
     ): ResponseEntity<*> {
-        println("lefutott $typeId")
+        val typeIdLong = typeId.trim().replace("\"", "").toLongOrNull()
+        val trimmedType = type.trim().replace("\"", "")
+
+        if (typeIdLong == null) {
+            return ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST)
+        }
 
         val verification = AuthUtils.verifyToken(token)
         if (!verification.verified) {
             return ResponseEntity<HttpStatus>(HttpStatus.UNAUTHORIZED)
         }
 
-
         return try {
-            val typeIdLong = typeId/*.replace("\"\"", "\"").toLong()*/
             var imageDirectory = ""
+            var imageDirectoryName = ""
             var imagePathToSave = ""
 
             val directory = File(Constants.IMAGE_PATH)
@@ -48,15 +54,17 @@ class ResourceController(private val placeRepository: PlaceRepository, private v
                 directory.mkdir()
             }
 
-            if (type == "place") {
+            if (trimmedType == "place") {
                 imageDirectory = Constants.IMAGE_PLACE_PATH
-                val directoryPlaces = File(Constants.IMAGE_PLACE_PATH)
+                imageDirectoryName = Constants.IMAGE_PLACE_PATH_NAME
+                val directoryPlaces = File(imageDirectory)
                 if (!directoryPlaces.exists()) {
                     directoryPlaces.mkdir()
                 }
 
-            } else if (type == "user") {
+            } else if (trimmedType == "user") {
                 imageDirectory = Constants.IMAGE_USER_PATH
+                imageDirectoryName = Constants.IMAGE_USER_PATH_NAME
                 val directoryPlaces = File(Constants.IMAGE_USER_PATH)
                 if (!directoryPlaces.exists()) {
                     directoryPlaces.mkdir()
@@ -64,20 +72,23 @@ class ResourceController(private val placeRepository: PlaceRepository, private v
             }
 
             val extension = StringUtils.getFilenameExtension(file.originalFilename)
-            val newFileName = RandomString(8)
-            val fileNameAndPath: Path = Paths.get(imageDirectory, "$newFileName.$extension")
-            Files.write(fileNameAndPath, file.bytes)
-            imagePathToSave = fileNameAndPath.toString()
+            if (extension == "jpg" || extension == "png") {
+                val newFileName = RandomString.make(8)
+                val fileNameAndPath: Path = Paths.get(imageDirectory, "$newFileName.$extension")
+                Files.write(fileNameAndPath, file.bytes)
+                imagePathToSave = "$imageDirectoryName-$newFileName.$extension"
+            }
 
-            if (type == "place") {
+            if (trimmedType == "place") {
                 val placeOptional = placeRepository.findById(typeIdLong)
                 if (placeOptional.isPresent) {
+
                     val place = placeOptional.get()
                     place.image = imagePathToSave
                     placeRepository.save(place)
                     ResponseEntity<HttpStatus>(HttpStatus.CREATED)
                 }
-            } else if (type == "user") {
+            } else if (trimmedType == "user") {
                 val userOptional = userRepository.findById(typeIdLong)
                 if (userOptional.isPresent) {
                     val user = userOptional.get()
@@ -93,4 +104,36 @@ class ResourceController(private val placeRepository: PlaceRepository, private v
         }
     }
 
+    @GetMapping()
+    fun getResource(
+            @RequestParam("image") imagePath: String,
+            @RequestParam(AuthUtils.COOKIE_NAME) token: String?
+    ): ResponseEntity<*> { // UrlResource + HttpStatus
+
+        val verification = AuthUtils.verifyToken(token)
+        if (!verification.verified) {
+            return ResponseEntity<HttpStatus>(HttpStatus.UNAUTHORIZED)
+        }
+
+        val extension = StringUtils.getFilenameExtension(imagePath)
+        val splits = imagePath.split('-')
+        val directory = splits[0]
+        val filename = splits[1]
+        if (directory != "users" && directory != "places") {
+            return ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST)
+        }
+        val path: Path = Paths.get("images/$directory/$filename")
+
+        val image = File(path.toUri())
+        if (!image.exists()) {
+            return ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND)
+        }
+
+        val urlRes = UrlResource(path.toUri())
+        val contentType: MediaType = if (extension == "jpg") MediaType.IMAGE_JPEG else MediaType.IMAGE_PNG
+
+        return ResponseEntity.ok()
+                .contentType(contentType)
+                .body(urlRes)
+    }
 }
--- src/main/kotlin/hu/zsof/restaurantApp/util/Constants.kt
+++ src/main/kotlin/hu/zsof/restaurantApp/util/Constants.kt
@@ -5,4 +5,7 @@ object Constants {
     const val IMAGE_PATH = "images"
     const val IMAGE_PLACE_PATH = "images/places"
     const val IMAGE_USER_PATH = "images/users"
+
+    const val IMAGE_PLACE_PATH_NAME = "places"
+    const val IMAGE_USER_PATH_NAME = "users"
 }
